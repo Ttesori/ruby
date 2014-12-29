fe60f7c2ac5f3b8c0617dfbd7bf382d6307876ba
Toni <tt@tontesori.com>
2014-12-29 18:41:39 -0500

    finished better bubble sort method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -3,36 +3,15 @@
 # bubble_sort([4,3,78,2,0,2])  => [0,2,2,3,4,78]
 
 def bubble_sort(to_sort)
-	# decide the passes_left: to_sort.length - 1
-	pass_number = 0
-	passes_left = to_sort.length - 1
-
-	# decide number of array compares_left each pass: to_sort.length
-	compares_left = to_sort.length
-
-	# begin sort if pass_number is < passes_left
-	while pass_number < passes_left
-		pass_number += 1
-		
-	# until reaching the to_sort[compares_left] for this pass, 
-		while compares_left > to_sort.index(compares_left)
-		
-	# store to_sort[n] (curr_slot_value) and to_sort[n+1] (next_slot_value) and to_sort.index(to_sort[n]) curr_slot
-			curr_slot_value = to_sort[n]
-			next_slot_value = to_sort[n+1]
-			curr_slot = to_sort.index(to_sort[n])
-			
-	# if curr_slot_value > next_slot_value, swap spots to_sort[curr_slot] = next_slot_value and to_sort[curr_slot + 1] = curr_slot_value
-			if curr_slot_value > next_slot_value
-				to_sort[curr_slot] = next_slot_value
-				to_sort[curr_slot + 1] = curr_slot value
-			else	
+pass = 0 # keep track of number of passes -- we need array.length - 1 to be sure we have them all
+	while pass < to_sort.length
+		to_sort.each_with_index do |value,slot| 
+			if (value <=> to_sort[slot.to_i+1]) == 1 #if first value is larger
+				to_sort[slot],to_sort[slot+1] = to_sort[slot+1],to_sort[slot] #swap values
 			end
-			compares_left -= 1
 		end
-	# decrement passes_left
-	passes_left -= 1
-	# return sorted_array
+	pass += 1 #increment passes
 	end
+	return to_sort
 end
-# bubble_sort([4,3,78,2,0,2])  => [0,2,2,3,4,78]
+p bubble_sort([4,3,78,2,0,2])
