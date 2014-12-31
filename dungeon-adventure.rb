4c9f788fe0d7db4158d634ed135baa32a03e817c
Toni <tt@tontesori.com>
2014-12-30 20:34:27 -0500

    Beginning first OO dungeon game

---
+++ dungeon-adventure.rb
@@ -0,0 +1,19 @@
+#Dungeon adventure game from Beginning Ruby, c. 6
+
+class Dungeon
+  attr_accessor :player
+
+  def initialize(player_name)
+    @player = Player.new(player_name)
+    @rooms = []
+  end
+
+  def add_room(reference, name, description, connections)
+    @rooms << Room.new(reference, name, description, connections)
+  end
+
+  Player = Struct.new(:name,:location)
+
+  Room = Struct.new(:reference,:name,:description,:connections)
+
+end
